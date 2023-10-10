acd34bc0aad87fad4216e8748ef72b9abbaf0cde
mathiasburger <mburakyilmazer38@gmail.com>
2023-10-11 02:39:57 +0300

    thats enough

--- DAY-5/day_5_solutions.js
+++ DAY-5/day_5_solutions.js
@@ -1,6 +1,69 @@
-console.log("--------------Day-5----------------")
+console.log("--------------Day-5----------------");
 
 /* -------------------------------------------------------------------------- */
 /*                             Exercise : Level 1                             */
 /* -------------------------------------------------------------------------- */
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
 
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+let emptyArr = Array();
+let newArray = Array(8).fill(10);
+let len = countries.length;
+console.log(len);
+console.log(newArray.length);
+console.log("first item of Array : ", countries[0]);
+console.log("middle item of Array:", countries[Math.ceil((len - 1) / 2)]);
+console.log("last item of Array: ", countries[len - 1]);
+let mixedDataTypes = [12, "12", true, false, { a: "df" }, ["q"]];
+console.log(mixedDataTypes.length);
+let itCompanies = ["Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"];
+let itComp = itCompanies.join(" ").replaceAll(",", "").split(" ");
+console.log(itComp);
+console.log(
+  itComp[0],
+  itComp[(itComp.length - 1) / 2],
+  itComp[itComp.length - 1]
+);
+console.log(itComp.join(" ").toUpperCase())
+console.log(itComp.join(", ") +" are big IT companies.");
+console.log(itComp.includes("Google"))
+let result = itComp.indexOf("facebook")
+result != -1 ? console.log("facebook arraydedir"):console.log("facebook arrayda değildir")
+console.log(itComp.sort())
+console.log(itComp.reverse())
+console.log(itComp)
+console.log(itComp.slice(0,3))
+console.log(itComp)
+console.log(itComp.slice(itComp.length-3,itComp.length))
+console.log(itComp.slice((itComp.length - 1) / 2, (itComp.length - 1) / 2+1));
+console.log(itComp.shift())
+console.log(itComp)
+console.log(itComp.splice(Math.floor((itComp.length-1)/2),1))
+console.log(itComp)
+console.log(itComp.pop())
+console.log(itComp.splice(0,itComp.length))
+console.log(itComp)
+
+// console.log(itComp.splice((itComp.length - 1) / 2),1);
+// console.log(itComp
